076a1b1e1c115cf9ed96b4f27b39e9e8880f89d7
Bhumana Balachandra Reddy <120649203+Balachandra2003@users.noreply.github.com>
2025-12-04 11:27:04 +0530

    Add build and push script for Docker image

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Building normal image..."
+docker build -t ghcr.io/my-org/remediation-image:latest .
+
+echo "Pushing image..."
+docker push ghcr.io/my-org/remediation-image:latest
 
